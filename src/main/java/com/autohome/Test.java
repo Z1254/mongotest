3c8ff1dac109cf964a6f3b05d3ef04c1a0fba0e3
zzn <122925636@qq.com>
2020-02-24 15:31:10 +0800

    测试

--- src/main/java/com/autohome/Test.java
+++ src/main/java/com/autohome/Test.java
@@ -9,11 +9,14 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
 import com.mongodb.client.model.changestream.FullDocument;
 import com.mongodb.client.model.changestream.OperationType;
+import com.sun.xml.internal.ws.developer.Serialization;
+import org.bson.BsonDocument;
 import org.bson.Document;
 
 import java.nio.channels.Channel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @author zzn
@@ -33,23 +36,62 @@ public class Test {
         addresses.add(address3);
         MongoClient client = new MongoClient(addresses);
         MongoDatabase test = client.getDatabase("test");
-        studentCollection = test.getCollection("Student");
+        studentCollection = test.getCollection("student");
         studentIncrCollection = test.getCollection("IncrStudent");
     }
 
     public static void main(String[] args) {
         //preInsertData();
         //开启监听任务
+        //listen_before();
+
+        listen_after();
+    }
+
+    private static void listen_before(){
         MongoCursor<ChangeStreamDocument<Document>> iterator = studentCollection.watch().fullDocument(FullDocument.UPDATE_LOOKUP).iterator();
         while (iterator.hasNext()){
             ChangeStreamDocument<Document> changeEvent = iterator.next();
             OperationType type = changeEvent.getOperationType();
-            System.out.println(type);
             if (type == OperationType.INSERT || type == OperationType.DELETE || type == OperationType.UPDATE || type == OperationType.REPLACE){
-                Document incrDocc = new Document();
-                incrDocc.append("filed_key",changeEvent.getDocumentKey().get("_id"));
-                incrDocc.append("filed_date",changeEvent.getFullDocument());
-                studentIncrCollection.insertOne(incrDocc);
+                Document incrDoc = new Document();
+                incrDoc.append("变更事件主体",changeEvent.toString());
+                incrDoc.append("变更类型type",changeEvent.getOperationType().toString());
+                incrDoc.append("变更文档的键值documentKey",changeEvent.getDocumentKey());
+                incrDoc.append("主键filed_key",changeEvent.getDocumentKey().get("_id"));
+                incrDoc.append("文档内容filed_date",changeEvent.getFullDocument());
+                incrDoc.append("Mongo命名空间",changeEvent.getNamespace().toString());
+                incrDoc.append("Token",changeEvent.getResumeToken());
+                System.out.println(changeEvent.getResumeToken());
+                //incrDoc.append("变更描述UpdateDescription",changeEvent.getUpdateDescription().toString());
+                //incrDoc.append("变更中更新字段UpdatedFields",changeEvent.getUpdateDescription().getUpdatedFields().toString());
+                //incrDoc.append("变更中删除字段getRemovedFields",changeEvent.getUpdateDescription().getRemovedFields());
+                studentIncrCollection.insertOne(incrDoc);
+            }
+        }
+    }
+
+    private static void listen_after(){
+        String Json = "{ \"_data\" : { \"$binary\" : \"gl5TeNkAAAABRmRfaWQAZF5TeNuWKsoNXDavvQBaEATGHxyL4y9C1pjWPxKxbm2zBA==\", \"$type\" : \"00\" } }";
+        BsonDocument parse = BsonDocument.parse(Json);
+        MongoCursor<ChangeStreamDocument<Document>> iterator = studentCollection.watch().resumeAfter(parse).iterator();
+        while (iterator.hasNext()){
+            ChangeStreamDocument<Document> changeEvent = iterator.next();
+            OperationType type = changeEvent.getOperationType();
+            if (type == OperationType.INSERT || type == OperationType.DELETE || type == OperationType.UPDATE || type == OperationType.REPLACE){
+                Document incrDoc = new Document();
+                incrDoc.append("变更事件主体",changeEvent.toString());
+                incrDoc.append("变更类型type",changeEvent.getOperationType().toString());
+                incrDoc.append("变更文档的键值documentKey",changeEvent.getDocumentKey());
+                incrDoc.append("主键filed_key",changeEvent.getDocumentKey().get("_id"));
+                incrDoc.append("文档内容filed_date",changeEvent.getFullDocument());
+                incrDoc.append("Mongo命名空间",changeEvent.getNamespace().toString());
+                incrDoc.append("Token",changeEvent.getResumeToken());
+                System.out.println(changeEvent.getResumeToken());
+                //incrDoc.append("变更描述UpdateDescription",changeEvent.getUpdateDescription().toString());
+                //incrDoc.append("变更中更新字段UpdatedFields",changeEvent.getUpdateDescription().getUpdatedFields().toString());
+                //incrDoc.append("变更中删除字段getRemovedFields",changeEvent.getUpdateDescription().getRemovedFields());
+                studentIncrCollection.insertOne(incrDoc);
             }
         }
     }
